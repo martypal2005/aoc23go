9f4d23d3f2249b8d8a1a803a54a8fae628674fb0
Martin Olsen <martypal2005@gmail.com>
2023-12-07 14:20:16 -0800

    latest

--- src/dec03/part1/main.go
+++ src/dec03/part1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"unicode"
@@ -11,7 +12,17 @@ const filePath = "../../../data/dec3/testinput.txt"
 
 type numIndex struct {
 	number int
-	index  int
+	start  int
+	end    int
+}
+
+// Check if symbol
+func isSymbol(s string, index int) bool {
+	r := []rune(s)
+	if index < 0 || index == len(r) {
+		return false // or handle error
+	}
+	return !unicode.IsDigit(r[index]) && r[index] != '.'
 }
 
 // Generator function: get the numbers and index
@@ -21,12 +32,16 @@ func getNumbers(s string) <-chan numIndex {
 		defer close(channel)
 		numStr := ""
 		index := 0
+		count := 0
+		first := true
 		for i, r := range s {
 			if unicode.IsDigit(r) {
-				if index == 0 {
+				if first {
 					index = i
+					first = false
 				}
 				numStr += string(r)
+				count++
 			} else {
 				if numStr == "" {
 					continue
@@ -35,9 +50,11 @@ func getNumbers(s string) <-chan numIndex {
 				if err != nil {
 					panic(err)
 				}
-				channel <- numIndex{num, index}
+				channel <- numIndex{num, index - 1, index + count}
 				index = 0
 				numStr = ""
+				count = 0
+				first = true
 			}
 		}
 
@@ -62,11 +79,59 @@ func main() {
 		lines = append(lines, line)
 	}
 
+	total := 0
+
 	for i := 0; i < len(lines); i++ {
 		for ni := range getNumbers(lines[i]) {
 
+			// Is there a symbol to the left?
+			if isSymbol(lines[i], ni.start) {
+				total += ni.number
+				continue
+			}
+
+			// Is there a symbol to the right?
+			if isSymbol(lines[i], ni.end) {
+				total += ni.number
+				continue
+			}
+			/*
+
+				// Check all indexes in line above
+				if i > 0 {
+					isPart := false
+					for j := ni.start; j < ni.end; j++ {
+						if isSymbol(lines[i-1], i) {
+							total += ni.number
+							isPart = true
+							break
+						}
+					}
+					if isPart {
+						continue
+					}
+				}
+
+					// Check all indexes in line below
+					if i < len(lines) {
+						isPart := false
+						for j := ni.start; j < ni.end; j++ {
+							if isSymbol(lines[i+1], i) {
+								total += ni.number
+								isPart = true
+								break
+							}
+						}
+						if isPart {
+							continue
+						}
+					}
+			*/
+
 		}
 	}
+
+	fmt.Println(total)
 
 	// Check if error reading file
 	if err := scanner.Err(); err != nil {
